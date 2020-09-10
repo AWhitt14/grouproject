78a32288ce1baf6c572a4ff1e5429b4ba94aa331
EmilyLow <emlow@me.com>
2020-09-09 20:13:10 -0500

    Emily edit

--- group/src/App.js
+++ group/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Emily Low Edit
         </a>
       </header>
     </div>
